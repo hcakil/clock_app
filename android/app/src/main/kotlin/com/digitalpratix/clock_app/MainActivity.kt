a693be593d92ca3ecffa4cd4e148e5ac05d07e21
hcakil <cakilhazim@gmail.com>
2022-02-20 09:52:57 +0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/digitalpratix/clock_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digitalpratix.clock_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/homepage.dart
@@ -0,0 +1,108 @@
+import 'package:clock_app/constants/theme_data.dart';
+import 'package:clock_app/data.dart';
+import 'package:clock_app/enums.dart';
+import 'package:clock_app/models/menu_info.dart';
+import 'package:clock_app/views/alarm_page.dart';
+import 'package:clock_app/views/clock_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Disables oriantation changes for application
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    return Scaffold(
+      backgroundColor: CustomColors.pageBackgroundColor,
+      body: Row(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: menuItems
+                .map((currentMenuInfo) => buildMenuButton(currentMenuInfo))
+                .toList(),
+          ),
+          VerticalDivider(
+            color: CustomColors.dividerColor,
+            width: 1,thickness: 1,
+          ),
+          Expanded(
+            child: Consumer<MenuInfo>(
+              builder: (BuildContext context, MenuInfo value, Widget child) {
+                if (value.menuType == MenuType.clock)
+                  return ClockPage();
+                else if (value.menuType == MenuType.alarm)
+                  return AlarmPage();
+                else
+                  return Container(
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(fontSize: 20),
+                        children: <TextSpan>[
+                          TextSpan(text: 'BLANK PAGE\n'),
+                          TextSpan(
+                            text: value.title,
+                            style: TextStyle(fontSize: 48),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildMenuButton(MenuInfo currentMenuInfo) {
+    return Consumer<MenuInfo>(
+      builder: (BuildContext context, MenuInfo value, Widget child) {
+        return FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(32))),
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 0),
+          color: currentMenuInfo.menuType == value.menuType
+              ? CustomColors.menuBackgroundColor
+              : Colors.transparent,
+          onPressed: () {
+            var menuInfo = Provider.of<MenuInfo>(context, listen: false);
+            menuInfo.updateMenu(currentMenuInfo);
+          },
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                currentMenuInfo.imageSource,
+                scale: 1.5,
+              ),
+              SizedBox(height: 16),
+              Text(
+                currentMenuInfo.title ?? '',
+                style: TextStyle(
+                    fontFamily: 'avenir',
+                    color: CustomColors.primaryTextColor,
+                    fontSize: 14),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
